4b49a3df2633418ab082c6a0a91d7d571f256bda
bloggerlucky43 <bloggerlucky43@gmail.com>
2025-04-19 13:11:10 +0100

    Previous index.js

--- index.js
+++ index.js
@@ -69,6 +69,75 @@ app.get('/verify-payment', async (req, res) => {
   console.log("The session id",sessionId);
   
 
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import OpenAI from "openai";  // Adjusted import for the latest package
+import Stripe from "stripe";
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2023-10-16' });
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+console.log(process.env.NODE_ENV)
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+ 
+
+app.post('/create-checkout-session', async (req, res) => {
+  const formData=req.body;
+  console.log("THE data fromthe frontend is",formData);
+  
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price: process.env.PRICE_ID,
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'https://divinemyst.life/success?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: 'https://divinemyst.life',
+      metadata: {
+        dob: formData.dob,
+        name: formData.name,
+        location: formData.location,
+        desire: formData.desire,
+        sign: formData.sign,
+        gender: formData.gender,
+      },
+    });
+
+    console.log("Saved data with session ID:", session.id);
+
+    res.json({ url: session.url });
+
+    // res.json({ sessionId: session.id });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get('/verify-payment', async (req, res) => {
+  const sessionId=req.query.session_id;
+
+  console.log("The session id",sessionId);
+  
+
   try {
     // Retrieve the Stripe checkout session to verify the payment
     const session = await stripe.checkout.sessions.retrieve(sessionId);
@@ -140,6 +209,14 @@ app.get('/verify-payment', async (req, res) => {
   }
 });
 
+
+
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/dist', 'index.html'));
+// });
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 
